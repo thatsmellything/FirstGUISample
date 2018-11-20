9d7d039c131130deb052ef7a50caddab08066276
Jensen Judkins <thesmelliestman@gmail.com>
2018-11-20 09:50:52 -0700

    This shuld now create a GUI and have button that is able to change the color of the background

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,12 +1,18 @@
 package gui.controller;
 import gui.model.Duck;
+import gui.view.FirstFrame;
+import gui.view.*;
 public class GUIController
 {
 
 	private Duck myDuck;
+	private FirstFrame appFrame;
+	private FirstPanel appLayout;
 	public GUIController()
 	{
 		myDuck = new Duck();
+		appFrame = new FirstFrame(this);
+		appLayout = new FirstPanel(this);
 	}
 	
 	public void start()
--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,6 +1,37 @@
 package gui.view;
 
-public class FirstFrame
+import javax.swing.JFrame;
+//dont forget to import the JFrame for the frame and the JPanel for the panel
+import gui.controller.GUIController;
+//dont forget to import the other files that are needed when linking multiple files
+
+public class FirstFrame extends JFrame
 {
 
+	private GUIController appController;
+	private FirstPanel appPanel;
+	//gotta create data members for files that you are linking to
+	
+	public FirstFrame(GUIController appController)
+	{
+		super();
+		//This is always needed when you use the extends JFrame thing at the top were the 
+		//class name thing is
+		this.appController = appController;
+		this.appPanel = new FirstPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		//this sets up the bases for the frame
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("My GUI");
+		this.setResizable(true);
+		this.setVisible(true);
+		//You must always have the setVisible to true otherwise you wont ever see your
+		// actual gui
+	}
 }
